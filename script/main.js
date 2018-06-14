98609bd4c0fda62c85c4e317b981020f65316a9f
ghost-fk <fayyad_r22@yahoo.com>
2018-06-14 15:05:06 +0200

    add eslint and fex the bug

--- script/main.js
+++ script/main.js
@@ -1,40 +1,44 @@
-function getRandomInt (min, max) {
-  min = Math.floor(min)
-  max = Math.ceil(max)
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+(function(){
+    function getRandomInt (min, max) {
+        min = Math.floor(min);
+        max = Math.ceil(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
-let firstDice={
-number:1,
-roll: () => {
-this.value = getRandomInt(1, 6)
-return this.value;
-}
-}
+    let firstDice={
+        number:getRandomInt(1, 6),
+        roll: () => {
+            this.number = getRandomInt(1, 6);
+            return this.number;
+        }
+    };
 
-let scundDice={
-  number:1,
-roll: () => {
-    this.value = getRandomInt(1, 6)
-    return this.value;
-}
-}
+    let scundDice={
+        number:getRandomInt(1, 6),
+        roll: () => {
+            this.number = getRandomInt(1, 6);
+            return this.number;
+        }
+    };
 
-function imagesToPrint (img1, img2) {
-let firstImag = document.querySelector('#first-dice');
-let scundImg = document.querySelector('#scund-dice');
-firstImag.style['background-image'] = `url(images/inverted-dice-${img1}.svg)`;
-scundImg.style['background-image'] = `url(images/inverted-dice-${img2}.svg)`;
-}
+    function imagesToPrint (img1, img2) {
+        let firstImag = document.querySelector('#first-dice');
+        let scundImg = document.querySelector('#scund-dice');
+        firstImag.style['background-image'] = `url(images/inverted-dice-${img1}.svg)`;
+        scundImg.style['background-image'] = `url(images/inverted-dice-${img2}.svg)`;
+    }
 
-let button = document.querySelector('#button-roll');
-button.addEventListener('click', event => {
-  event.preventDefault()
-  let firstroll = firstDice.roll()
-  let scundroll = scundDice.roll();
+    let button = document.querySelector('#button-roll');
+    button.addEventListener('click', event => {
+        event.preventDefault();
+        let firstroll = firstDice.roll();
+        let scundroll = scundDice.roll();
 
-imagesToPrint(firstroll, scundroll);
-})
+        imagesToPrint(firstroll, scundroll);
+    });
 
 
-imagesToPrint(firstDice.value, scundDice.value);
+    imagesToPrint(firstDice.number, scundDice.number);
+
+
+})();
